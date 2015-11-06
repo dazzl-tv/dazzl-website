6224b0d84fe2b413e6441ebc825dbad12074b3d8
Rom-BZH <romain.faust@epitech.eu>
2015-11-06 14:29:22 +0100

    Update scrolling-nav.js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -10,6 +10,7 @@ $(window).scroll(function() {
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+        var parent = $(this).closest("div").removeClass("in").attr("aria-expanded", false);
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
